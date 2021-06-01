a6635bf675829dd90f90b2030b5d9e195c02f3bd
Sajjad Ahmad <jogimar14@gmail.com>
2021-06-01 16:02:59 +0500

    Pass name property for every button to Button component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
-import Button from './Button'
-export default class ButtonPanel extends Component {
+import React from 'react';
+import Button from './Button';
 
-  render() {
-    return <div>
-    <Button
-      zero =  {'0'}
-      one =  {'1'}
-      two =  {'2'}
-      three =  {'3'}
-      four =  {'4'}
-      five =  {'5'}
-      six =  {'6'}
-      seven =  {'7'}
-      eigth =  {'8'}
-      nine =  {'9'}
-      add =  {'+'}
-      substract =  {'-'}
-      multiply =  {'X'}
-      devide =  {'/'}
-      modulo =  {'%'}
-      equal  =  {'='}
-      extra =  {'AC'}
-      extra2 =  {'+/-'}
-      point = {'.'}
-      c = {'C'}
-    />
+const ButtonPanel = () => (
+  <div className="btn-panel">
+    <div className="group-1">
+      <Button name="AC" />
+      <Button name="+/-" />
+      <Button name="%" />
+      <Button name="/" />
     </div>
-  }
-}
+    <div className="group-2">
+      <Button name="7" />
+      <Button name="8" />
+      <Button name="9" />
+      <Button name="X" />
+    </div>
+    <div className="group-3">
+      <Button name="4" />
+      <Button name="5" />
+      <Button name="6" />
+      <Button name="-" />
+    </div>
+    <div className="group-4">
+      <Button name="1" />
+      <Button name="2" />
+      <Button name="3" />
+      <Button name="+" />
+    </div>
+    <div className="group-5">
+      <Button name="0" />
+      <Button name="." />
+      <Button name="C" />
+      <Button name="=" />
+    </div>
+  </div>
+);
+
+export default ButtonPanel;

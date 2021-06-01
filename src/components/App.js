578d77570b5090e6c7b8a84aff4cba8ad94b8248
Sajjad Ahmad <jogimar14@gmail.com>
2021-06-01 16:05:06 +0500

    Convert class components to functional components to avoid eslint errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-function App() {
-  return (
-    <div className = 'main-app'>
-    <Display/>
-    <ButtonPanel/>
-    </div>
-  );
-}
+const App = () => (
+  <div className="main-app">
+    <Display />
+    <ButtonPanel />
+  </div>
+);
 
 export default App;
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Display extends Component {
-  render() {
-    return <div className = 'display-board'>
-    <h1 className = 'result'>{this.props.result }</h1>
-    </div>
-  }
-}
+const Display = ({ result }) => (
+  <div className="display-board">
+    <h1 className="result">
+      {result}
+    </h1>
+  </div>
+);
 
-  Display.propTypes = { 
-    result: PropTypes.string 
-  }
+Display.propTypes = {
+  result: PropTypes.string,
+};
 
-  Display.defaultProps = {
-    result: 0,
-  }
+Display.defaultProps = {
+  result: 0,
+};
+
+export default Display;

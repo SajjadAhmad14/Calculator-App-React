eabb97042073189ce97aff9e8f6c10a4a9e91505
Sajjad Ahmad <jogimar14@gmail.com>
2021-06-07 13:57:59 +0500

    Make function a class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <div className="main-app">
-    <Display />
-    <ButtonPanel />
-    <calculate />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const data = { total: null, next: null, operation: null };
+    const { total, next, operation } = calculate(data, buttonName);
+    this.setState({ total, next, operation });
+  }
+
+  render() {
+    return (
+      <div className="main-app">
+        <Display result ={this.state.total} />
+        <ButtonPanel onClick={this.handleClick} />
+        {/* <calculate /> */}
+      </div>
+    );
+  }
+}
 
 export default App;

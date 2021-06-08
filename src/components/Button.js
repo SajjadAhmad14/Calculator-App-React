b48f7668b29acf81309928df82371b1ed582831f
Sajjad Ahmad <jogimar14@gmail.com>
2021-06-08 19:47:48 +0500

    Attach handleClick event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name, buttonClass }) => (
-  <button type="button" className={buttonClass}>
-    {name}
-  </button>
-);
+const Button = ({ name, buttonClass, clickHandler }) => {
+  const handleClick = () => {
+    clickHandler(name);
+  };
 
+  return (
+    <button type="button" className={buttonClass} onClick={handleClick}>
+      {name}
+    </button>
+  );
+};
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   buttonClass: PropTypes.string.isRequired,
+};
+
+Button.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Button;
